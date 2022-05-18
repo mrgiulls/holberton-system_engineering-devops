e4f5a7a322b35dcd4ea3936608ef2b67d578f2cc
mrgiulls <giuliano.mesias@hotmail.com>
2022-05-18 00:33:22 -0500

    readme

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+Consume API with Python
+'''
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                     .format(sys.argv[1]))
+    d = r.json()
+    name = d.get('name')
+    user_id = d.get('id')
+    r = requests.get('https://jsonplaceholder.typicode.com/todos')
+    d_todos = r.json()
+    tasks, completed = 0, 0
+    titles = []
+    for i in d_todos:
+        if i.get('userId') == user_id:
+            tasks += 1
+            if i.get('completed'):
+                completed += 1
+                titles.append(i.get('title'))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, completed, tasks))
+    for i in titles:
+        print('\t', i)
